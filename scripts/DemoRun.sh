fb3b9539ec367b3d37b82b5b15d2bb03a5f69eeb
Graham Land <allthingsclowd@users.noreply.github.com>
2020-05-18 01:15:08 +0100

    Update DemoRun.sh

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./Generate_PKI_Certificates_For_Lab.sh consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4"
-./Generate_PKI_Certificates_For_Lab.sh nomad "server.global.nomad" "client.global.nomad" "1.2.3.4"
-./Generate_PKI_Certificates_For_Lab.sh vault "server.global.vault" "client.global.vault" "1.2.3.4"
+/usr/local/bootstrap/scripts/Generate_PKI_Certificates_For_Lab.sh consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4"
+/usr/local/bootstrap/scripts/Generate_PKI_Certificates_For_Lab.sh nomad "server.global.nomad" "client.global.nomad" "1.2.3.4"
+/usr/local/bootstrap/scripts/Generate_PKI_Certificates_For_Lab.sh vault "server.global.vault" "client.global.vault" "1.2.3.4"
