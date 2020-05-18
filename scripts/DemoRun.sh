a3ac79f5ceaad1cc0fb4d1320df6b613cb96e64a
Graham Land <allthingsclowd@users.noreply.github.com>
2020-05-18 21:38:12 +0100

    Update DemoRun.sh

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/local/bootstrap/scripts/Generate_PKI_Certificates_For_Lab.sh consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4"
-/usr/local/bootstrap/scripts/Generate_PKI_Certificates_For_Lab.sh nomad "server.global.nomad" "client.global.nomad" "1.2.3.4"
-/usr/local/bootstrap/scripts/Generate_PKI_Certificates_For_Lab.sh vault "server.global.vault" "client.global.vault" "1.2.3.4"
+wget -O - https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/master/scripts/Generate_PKI_Certificates_For_Lab.sh | bash -s consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4" 
+wget -O - https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/master/scripts/Generate_PKI_Certificates_For_Lab.sh | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
+wget -O - https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/master/scripts/Generate_PKI_Certificates_For_Lab.sh | bash -s vault "server.global.vault" "client.global.vault" "1.2.3.4" 
+
