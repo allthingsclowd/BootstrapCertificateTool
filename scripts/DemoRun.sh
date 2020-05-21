7ed9dc9f308bee2f096f05419e4408c519ecf793
Graham Land <allthingsclowd@users.noreply.github.com>
2020-05-21 21:17:14 +0100

    Update DemoRun.sh

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export BootStrapCertTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/0.0.1/scripts/Generate_PKI_Certificates_For_Lab.sh"
+export BootStrapCertTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/allthingsclowd-patch-1/scripts/Generate_PKI_Certificates_For_Lab.sh"
 
 wget -O - ${BootStrapCertTool} | bash -s consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4" 
 wget -O - ${BootStrapCertTool} | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
