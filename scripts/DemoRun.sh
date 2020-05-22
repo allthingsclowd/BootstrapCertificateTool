2af0008660d3a50e41ccd687b196747ae05b3376
allthingsclowd <graham@allthingscloud.eu>
2020-05-22 23:38:56 +0100

    t99

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-export BootStrapCertTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/0.0.1/scripts/Generate_PKI_Certificates_For_Lab.sh"
+# export BootStrapCertTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/0.0.1/scripts/Generate_PKI_Certificates_For_Lab.sh"
 
-# export BootStrapCertTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/move_to_env_vars/scripts/Generate_PKI_Certificates_For_Lab.sh"
-
+export BootStrapCertTool="https://github.com/allthingsclowd/BootstrapCertificateTool/blob/master/scripts/Generate_PKI_Certificates_For_Lab.sh"
 
 wget -O - ${BootStrapCertTool} | bash -s consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4" 
 wget -O - ${BootStrapCertTool} | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
