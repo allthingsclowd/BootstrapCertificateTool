7e22c39f6e44a2c11a03cc066c6006effb7aef8b
allthingsclowd <graham@allthingscloud.eu>
2020-05-26 15:27:15 +0100

    t

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -5,8 +5,8 @@
 export BootStrapCertTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/whoopie/scripts/Generate_PKI_Certificates_For_Lab.sh"
 
 wget -O - ${BootStrapCertTool} | bash -s consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4" 
-wget -O - ${BootStrapCertTool} | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
-wget -O - ${BootStrapCertTool} | bash -s vault "server.global.vault" "client.global.vault" "1.2.3.4" 
-wget -O - ${BootStrapCertTool} | bash -s bastion "server.global.bastion" "client.global.bastion" "1.2.3.4"
-wget -O - ${BootStrapCertTool} | bash -s ssh "server.global.ssh" "client.global.ssh" "1.2.3.4" 
+# wget -O - ${BootStrapCertTool} | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
+# wget -O - ${BootStrapCertTool} | bash -s vault "server.global.vault" "client.global.vault" "1.2.3.4" 
+# wget -O - ${BootStrapCertTool} | bash -s bastion "server.global.bastion" "client.global.bastion" "1.2.3.4"
+# wget -O - ${BootStrapCertTool} | bash -s ssh "server.global.ssh" "client.global.ssh" "1.2.3.4" 
 
