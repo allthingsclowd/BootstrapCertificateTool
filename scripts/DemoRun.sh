a9a5c2e2cfe0b8644ed8dafc29bb8674fde73687
allthingsclowd <graham@allthingscloud.eu>
2020-05-29 18:02:17 +0100

    q

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -6,10 +6,10 @@ export BootstrapSSHTool="https://github.com/allthingsclowd/BootstrapCertificateT
 
 # Generate OpenSSH Certs
 wget -O - ${BootstrapSSHTool} | bash -s "ssh" "grazzer"
-wget -O - ${BootstrapSSHTool} | bash -s "bastion" "grazzer"
-# Generate OpenSSL Certs
-wget -O - ${BootStrapCertTool} | bash -s consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4" 
-wget -O - ${BootStrapCertTool} | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
-wget -O - ${BootStrapCertTool} | bash -s vault "server.global.vault" "client.global.vault" "1.2.3.4" 
-wget -O - ${BootStrapCertTool} | bash -s wpc "server.global.wpc" "client.global.wpc" "1.2.3.4"
+# wget -O - ${BootstrapSSHTool} | bash -s "bastion" "grazzer"
+# # Generate OpenSSL Certs
+# wget -O - ${BootStrapCertTool} | bash -s consul "server.node.global.consul" "client.node.global.consul" "1.2.3.4" 
+# wget -O - ${BootStrapCertTool} | bash -s nomad "server.global.nomad" "client.global.nomad" "1.2.3.4" 
+# wget -O - ${BootStrapCertTool} | bash -s vault "server.global.vault" "client.global.vault" "1.2.3.4" 
+# wget -O - ${BootStrapCertTool} | bash -s wpc "server.global.wpc" "client.global.wpc" "1.2.3.4"
 
--- scripts/Generate_Access_Certificates.sh
+++ scripts/Generate_Access_Certificates.sh
@@ -74,7 +74,7 @@ generate_host_certificates () {
     
     # Create new host keys if they don't already exist
     echo -e "Generate new ssh keys for ${1} HOST Key CERTIFICATES"
-    push $Certs_dir
+    pushd $Certs_dir
     # Generate new keys if required
     [ ! -f $Certs_dir/${1}_host_rsa_key ] && \
         ssh-keygen -N '' -C HASHISTACK-${1}-HOST-KEY -t rsa -b 4096 -h -f $Certs_dir/${1}/${1}_host_rsa_key && \
@@ -99,20 +99,21 @@ generate_host_certificates () {
     echo -e "\nNow configure the ssh clients to accept the Host Signed Certificate e.g. $Int_CA_dir/${1}/${1}-host-ca.pub \n"
     echo -e "\ngrep -qxF '@cert-authority * ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFmZo/bkvhmUEjx3erXC+rZ1R3htLHtz0VzZNpgQD2sT2KZLW3yBiKYIKgxICM04MQsVHY1k5y4ek/tgnw05m5KOO5KTHxxKjcBKf2EyvwG0o8vnzo6UgweqXEePigAzSGQfcsGp75tVu3qmeLKXtJOo1WaWmTSNH4Qoq89xRiPslCVDi1i2VEPxJi3+eeFL5WO+nBK9Xt28DaXY4B43sgC1KC6DSRUR2JhlgPGMKP2eTE5+UaEldyPVzdIl2j3tLsaURfr+cZ6ryPEE9phT1bjcOSC3A88NrROZH1FvpZpG6NQPXusTWjre/NIz2TdG44AopbFRKAEpMVFw67AJ6oDWHPTrh2TGh3SQEIIZTdhudZIHnwiSBuKUOqyV65KH/mmy5gr8X2miHbM+qh6ISjqwPN6TjAhUPgkjxtwa7K+tDseBoFsrRIgP65hHAIlEFodHUI8Lu3P5HswH39z8ouEDR+qU54z9JO/E0Mw9YQPk19A6jr7o9/06wqSXfkVmS1VwvyZI90Zqrtg4+lZ3Zq/GLDqpxTlakfEAddOd9Ns01GgeSab4mKDwB6r2VTsunXQ4DDJkzxm9ioJmX7Ctv9J50Hqqcv+kiM8jJHrsB4IIrc0Cc/qb08YAo//i44JTuPxs2+FS2ifDmQA+TK5fJxwUIQJ6KDQ+0wB+T6yeYMJw== HOST-CA' /etc/ssh/ssh_known_hosts || echo '@cert-authority * ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFmZo/bkvhmUEjx3erXC+rZ1R3htLHtz0VzZNpgQD2sT2KZLW3yBiKYIKgxICM04MQsVHY1k5y4ek/tgnw05m5KOO5KTHxxKjcBKf2EyvwG0o8vnzo6UgweqXEePigAzSGQfcsGp75tVu3qmeLKXtJOo1WaWmTSNH4Qoq89xRiPslCVDi1i2VEPxJi3+eeFL5WO+nBK9Xt28DaXY4B43sgC1KC6DSRUR2JhlgPGMKP2eTE5+UaEldyPVzdIl2j3tLsaURfr+cZ6ryPEE9phT1bjcOSC3A88NrROZH1FvpZpG6NQPXusTWjre/NIz2TdG44AopbFRKAEpMVFw67AJ6oDWHPTrh2TGh3SQEIIZTdhudZIHnwiSBuKUOqyV65KH/mmy5gr8X2miHbM+qh6ISjqwPN6TjAhUPgkjxtwa7K+tDseBoFsrRIgP65hHAIlEFodHUI8Lu3P5HswH39z8ouEDR+qU54z9JO/E0Mw9YQPk19A6jr7o9/06wqSXfkVmS1VwvyZI90Zqrtg4+lZ3Zq/GLDqpxTlakfEAddOd9Ns01GgeSab4mKDwB6r2VTsunXQ4DDJkzxm9ioJmX7Ctv9J50Hqqcv+kiM8jJHrsB4IIrc0Cc/qb08YAo//i44JTuPxs2+FS2ifDmQA+TK5fJxwUIQJ6KDQ+0wB+T6yeYMJw== HOST-CA' | sudo tee -a /etc/ssh/ssh_known_hosts"
     echo -e "SSH Host CA and Key creation process for ${1} is has completed."
+    popd
 }
 
 generate_new_user_keys () {
     
     # Create new host keys if they don't already exist
     echo -e "Generate new ssh keys for user ${1}"
-    push $Certs_dir
+    pushd $Certs_dir
     # Generate new keys if required
     ssh-keygen -N '' -C HASHISTACK-${1}-USER-KEY -t rsa -b 2048 -h -f $Certs_dir/${2}/${1}_${2}_user_rsa_key && \
         echo -e "\nNew SSH keys created - $Certs_dir/${2}/${1}_${2}_user_rsa_key, $Certs_dir/${2}/${1}_${2}_user_rsa_key.pub"
 
     echo -e "Sign the new keys for user ${1}"
     # Sign the user key with the public key
-    ssh-keygen -s $Int_CA_dir/${1}/${1}-client-ca -I hashistack_server -n ${2},root,vagrant,graham,pi -V -5:+52w -z 1 $Certs_dir/${2}/${1}_${2}_user_rsa_key.pub && \
+    ssh-keygen -s $Int_CA_dir/${2}/${2}-client-ca -I hashistack_server -n ${1},root,vagrant,graham,pi -V -5:+52w -z 1 $Certs_dir/${2}/${1}_${2}_user_rsa_key.pub && \
         echo -e "\nNew SSH CERTIFICATE created - $Certs_dir/${2}/${1}_${2}_user_rsa_key-cert.pub"      
 
     # Now copy ${1}_host_rsa_key.pub, ${1}_host_rsa_key-cert.pub and ${1}_host_rsa_key to the target system
@@ -122,6 +123,7 @@ generate_new_user_keys () {
 
     echo -e "\nNow copy $Certs_dir/${2}/${1}_${2}_user_rsa_key, $Certs_dir/${2}/${1}_${2}_user_rsa_key.pub & $Certs_dir/${2}/${1}_${2}_user_rsa_key-cert.pub to the client in the /home/someuser/.ssh directory"
     echo -e "${2} SSH USER CA and Key creation process for ${1} is has completed."
+    popd
 }
 
 echo -e "\n===================================================="
