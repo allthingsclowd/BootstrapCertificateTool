26c727e445c80d59cbab43a83b547ea7a339f983
Graham Land <allthingsclowd@users.noreply.github.com>
2020-06-01 00:02:17 +0100

    t

--- scripts/Generate_Access_Certificates.sh
+++ scripts/Generate_Access_Certificates.sh
@@ -43,24 +43,6 @@ setup_env () {
 
 }
 
-generate_host_ca_signing_keys () {
-    
-    # ${1} - Environment that host ca signing key is gemerated for 
-
-    echo -e "\n===================================================="
-    echo -e "Check to see if a SSH HOST CA KEY for ${1} already exists?"
-    # Generate a new OpenSSH CA if one does not already exist
-    
-    [ ! -f $Int_CA_dir/${1}/${1}-ssh-host-rsa-ca ] && \
-        ssh-keygen -t rsa -N '' -C ${1}-SSH-HOST-RSA-CA -b 4096 -f $Int_CA_dir/${1}/${1}-ssh-host-rsa-ca && \
-        echo -e "\nNew SSH CA created - $Int_CA_dir/${1}/${1}-ssh-host-rsa-ca" && \
-        echo "export ${1}_ssh_host_rsa_ca='`cat $Int_CA_dir/${1}/${1}-ssh-host-rsa-ca`'" \
-        >> ${Int_CA_dir}/BootstrapCAs.sh || \
-        echo -e "\nSSH CA found - $Int_CA_dir/${1}/${1}-ssh-host-rsa-ca - this will be re-used."
-    
-    [ -f ${Int_CA_dir}/BootstrapCAs.sh ] && source ${Int_CA_dir}/BootstrapCAs.sh
-}
-
 generate_new_ssh_host_keys () {
 
     # ${1} - Environment that host ca signing key is gemerated for
@@ -166,32 +148,6 @@ generate_new_user_keys () {
     popd
 }
 
-generate_user_ca_signing_keys () {
-    # ${1} - Environment that host ca signing key is gemerated for 
-
-    echo -e "\n===================================================="
-    echo -e "Check to see if a SSH USER CA KEY for ${1} already exists?"
-    # Generate a new OpenSSH CA if one does not already exist
-    
-    [ ! -f $Int_CA_dir/${1}/${1}-ssh-user-rsa-ca ] && \
-        ssh-keygen -t rsa -N '' -C ${1}-SSH-USER-RSA-CA -b 4096 -f $Int_CA_dir/${1}/${1}-ssh-user-rsa-ca && \
-        echo -e "\nNew SSH CA created - $Int_CA_dir/${1}/${1}-ssh-user-rsa-ca" && \
-        echo "export ${1}_ssh_user_rsa_ca='`cat $Int_CA_dir/${1}/${1}-ssh-user-rsa-ca`'" \
-        >> ${Int_CA_dir}/BootstrapCAs.sh || \
-        echo -e "\nSSH USER CA found - $Int_CA_dir/${1}/${1}-ssh-user-rsa-ca - this will be re-used."
-
-    [ -f /etc/ssh/${1}-ssh-user-rsa-ca.pub ] && rm -f /etc/ssh/${1}-ssh-user-rsa-ca.pub
-
-    cp $Int_CA_dir/${1}/${1}-ssh-user-rsa-ca.pub /etc/ssh/${1}-ssh-user-rsa-ca.pub
-    echo -e "\nConfigure the target system to Trust user certificates signed by the ${1}-SSH-USER-RSA-CA key when ssh certificates are used"
-    grep -qxF "TrustedUserCAKeys /etc/ssh/${1}-ssh-user-rsa-ca.pub" /etc/ssh/sshd_config || echo "TrustedUserCAKeys /etc/ssh/${1}-ssh-user-rsa-ca.pub" | sudo tee -a /etc/ssh/sshd_config
-
-    [ -f ${Int_CA_dir}/BootstrapCAs.sh ] && source ${Int_CA_dir}/BootstrapCAs.sh
-    echo -e "SSH User(Client) Key creation process for ${1} is has completed."
-
-}
-
-
 echo -e "\n===================================================="
 echo -e "\nStarting to create OpenSSH certificates for lab setup"
 echo -e "\n====================================================\n\n"
@@ -201,11 +157,7 @@ echo -e "\n====================================================\n\n"
 # ${2} - Username - e.g. grazzer
 
 setup_env ${1}
-
-generate_host_ca_signing_keys ${1}
 generate_new_ssh_host_keys ${1} ${3}
-
-generate_user_ca_signing_keys ${1}
 create_ssh_user ${2}
 generate_new_user_keys ${1} ${2}
 
