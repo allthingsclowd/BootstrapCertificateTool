2860e1677c864e594075c1e40c818058e526d76a
Graham Land <allthingsclowd@users.noreply.github.com>
2020-06-07 23:52:21 +0100

    t

--- scripts/BootStrapMe.sh
+++ scripts/BootStrapMe.sh
@@ -115,7 +115,9 @@ generate_and_configure_new_host_keys() {
     
 
     # Check that CA signing key is available
-    if [ ! -z "${NAME}_ssh_rsa_ca" ] || [ ! -z "${NAME}_ssh_rsa_ca_pub" ]; then
+    caEnv=${NAME}_ssh_rsa_ca
+    caPubEnv=${NAME}_ssh_rsa_ca_pub
+    if [ ! -z "${!caEnv}" ] || [ ! -z "${!caPubEnv}" ]; then
         
         # load the signing keys into memory
         if [ -f "${bootStrapFile}" ]; then
@@ -125,7 +127,7 @@ generate_and_configure_new_host_keys() {
           env
           echo "${NAME}_ssh_rsa_ca AND ${NAME}_ssh_rsa_ca_pub"
           
-          if [ ! -z "${NAME}_ssh_rsa_ca" ] || [ ! -z "${NAME}_ssh_rsa_ca_pub" ]; then
+          if [ ! -z "${!caEnv}" ] || [ ! -z "${!caPubEnv}" ]; then
             echo -e "BANG! No signing keys found in ${bootStrapFile} to commence bootstrap process"
             exit 1
           fi
