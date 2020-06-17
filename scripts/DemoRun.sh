70daef8e9e6c31c77a6293a7a32d1ca728fceb14
allthingsclowd <graham@allthingscloud.eu>
2020-06-17 16:39:54 +0100

    missing closing;

--- scripts/DemoRun.sh
+++ scripts/DemoRun.sh
@@ -54,12 +54,47 @@ source /usr/local/bootstrap/var.env
 # ~/vagrant_workspace/lab_certificate_creation (master)
 
 { [ -f BootstrapCAs.sh ] && source BootstrapCAs.sh; } || { echo -e "!!!!!!STOP!!!!!!\n Missing BootstrapCAs.sh file \n" && exit 1; }
-{ [ -f var.env ] && source var.env; } || { echo -e "!!!!!!STOP!!!!!!\n Missing var.env file \n" && exit 1 }
+{ [ -f var.env ] && source var.env; } || { echo -e "!!!!!!STOP!!!!!!\n Missing var.env file \n" && exit 1; }
 export BootstrapSSHTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/${certversion}/scripts/BootStrapMe.sh"
 # Generate OpenSSH Certs
 wget -O - ${BootstrapSSHTool} | bash -s - -H -n BASTIONHOST -h ${HOSTNAME} -s
 wget -O - ${BootstrapSSHTool} | bash -s - -U -n BASTIONUSER -u iac4me -b "graham,grazzer,pi,root" -s
 systemctl restart sshd
 
-# STEP 3 - On MacBook add BASTIONHOST and PACKERHOST CA certificates to the known_hosts file and verify all is well
+# STEP 3 - On MacBook add BASTIONHOST and PACKERHOST CA certificates to the known_hosts file and verify all is well by logging in
+
+# STEP 4 - Update the Packer build process to leverage the new ssh Keys in your packer pipeline
+# Add the public key for the PACKERUSER created above to the seed file used to deploy ubuntu silently - this is ALL OS specific, in this case ubuntu
+# Copy across the BootStrap.sh file from the labcert directory
+# Ensure the correct keys are in place
+# Review the Certify.sh script to ensure the versions are correct
+#
+# source /usr/local/bootstrap/var.env
+# source /usr/local/bootstrap/.bootstrap/CA/BootstrapCAs.sh
+
+
+# { [ -f /usr/local/bootstrap/.bootstrap/CA/BootstrapCAs.sh ] && source /usr/local/bootstrap/.bootstrap/CA/BootstrapCAs.sh; } || { echo -e "!!!!!!STOP!!!!!!\n Missing /usr/local/bootstrap/.bootstrap/CA/BootstrapCAs.sh file \n" && exit 1; }
+# { [ -f /usr/local/bootstrap/var.env ] && source /usr/local/bootstrap/var.env; } || { echo -e "!!!!!!STOP!!!!!!\n Missing /usr/local/bootstrap/var.env file \n" && exit 1 }
+# export BootstrapSSHTool="https://raw.githubusercontent.com/allthingsclowd/BootstrapCertificateTool/${certversion}/scripts/BootStrapMe.sh"
+# # Generate OpenSSH Certs
+# wget -O - ${BootstrapSSHTool} | bash -s - -H -n PACKERHOST -h ${HOSTNAME} -s
+# wget -O - ${BootstrapSSHTool} | bash -s - -U -n PACKERUSER -u packman -b "graham,grazzer,pi,root" -s
+
+# # Create grazzer user account.
+
+# d-i preseed/late_command string \
+#     in-target sed -i 's/^%sudo.*$/%sudo ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers; \
+#     in-target /bin/sh -c "echo 'Defaults env_keep += \"SSH_AUTH_SOCK\"' >> /etc/sudoers"; \
+#     in-target mkdir -p /home/packman/.ssh; \
+#     in-target /bin/sh -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCzgVzATNldsAtFrpXMMpz193itJ4dGK3YxU3eWQXxslQapukBhXyLlnyyw+qzi7H0PWjbCoHjQRUSMguyRhLF7IcXTpf/cF++u/tTEsejbnT2HOCS6obYfZKKF/4ZelRZXHv869mZZUoh+frUk5KxJPjx/4eYFYi2LY0SD//NmT+i6Ip/hfqe24anL/+uya+yZAilPMyPCpLhrd2LLW9tWun+rNRosAPT0h03zCABzS0cpizL7HxbgbnZxyWll5RLqpSigs9Gzk7BX3oedZhZrhKtmvQmhvvSnKhvCQMA6HgsFJ47zd5Cwj1WEwDaFkq8Fec7hFQoPjTT6T9OAHexoSRuQbg3cQcKu/tqSLZDcY8Y5HtpCz3bnhJOnC+1aznpbdoGSBeUJ43c2cIF/lsWgFkjz/I2IsAtrncV9MLQL+c89AJx0E4sJ9/5LsNFQo2FoGa1bewwxtJ/941AZvOP8U4gEja3De+KO3SqrU2rVs6c+J1yG9kMjHhyGORa9ULTosupKUYejA13A9plpbi/Uc6QVmAMTNzAGerwF4iklf3BCqyPzv/6eXU4yz4S834vzTY0lsgipDZSpSog1qWr9CNBe/nkEkawm50tuPZPK0pH1+buIUJZlYUe3USmEiuIg3M+hoEAROg+tc0HlR0KFhPWPt+aZdfbhEpGlFEeBew== packman-PACKERUSER-USER-KEY' >> /home/packman/.ssh/authorized_keys"; \
+#     in-target chown -R packman:packman /home/packman/; \
+#     in-target chmod -R go-rwx /home/packman/.ssh/authorized_keys; \
+#     in-target sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config; \
+#     in-target sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config;
+
+# STEP 5 - Update Vagrant Deployment Pipeline for Dev work
+
+# STEP 6 - Update Terraform code for local deployment
+
+# STEP 7 - [Optional] Migrate to TF Cloud and Bootstrap their too :)
 
